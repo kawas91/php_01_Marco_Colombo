06929fd39fab7d2b051b34fd563cd18b456caeae
Marco <colombomarco07@gmail.com>
2026-07-10 12:54:32 +0200

    Primo commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$numero = 1;
+$numeroVirgola = 1.5;
+$parola = 'Hello Word!';
+$check = true;
+
+var_dump($numero, $numeroVirgola, $parola, $check);
+
+
+$nome = 'Donato';
+
+echo  "$nome \n";
+
+$nome = 'Andrea';
+
+echo $nome;
